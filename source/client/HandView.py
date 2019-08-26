57f8dab3241b950ab07b5c973a6c9c8406db2f57
slwoodward <21129239+slwoodward@users.noreply.github.com>
2019-08-26 10:55:59 -0700

    GUI work.
    Created class "cardAndImage" to keep track of cards image and location in HandView.  Having trouble keeping it synced with most recent card (it lags one card).   Also propose making update to TableView as an event triggered by user.

--- source/client/HandView.py
+++ source/client/HandView.py
@@ -2,9 +2,25 @@ import pygame
 import textwrap
 from common.Card import Card
 import client.UIConstants as UIC
-import client.TableView as TV
+from client.TableView import TableView
 
 from PodSixNet.Connection import connection, ConnectionListener
+
+class cardAndImage():     
+        """GUI needs image and position of card"""
+        def __init__(self, card, xy, img):
+            # should we check that card is in deck?
+            self._card = card
+            self._img = cardAndImage.get_image(self._card)
+            print('do NOT want get_image in render loop.')
+            self._xy = xy      
+        def get_image(self):
+            temp = self._suit
+            temp = temp[0]
+            imageFileName='client.card'+str(self._number)+ temp + '.png'
+            print(imageFileName)
+            img = 'imageFileName: '+imageFileName  # To DO load image and make this image
+            return(img)                                             
 
 class HandView():
     """This class handles letting players actualy input information
@@ -31,7 +47,7 @@ class HandView():
         # 
         currentHand = self.controller.Get_Hand()
         self.Print_Text("{0}".format(currentHand), (UIC.publicPrivateBoundary,0))
-
+        # ---
         pygame.display.update()
 
     def Next_Event(self):
@@ -42,9 +58,26 @@ class HandView():
                 #The window crashed, we should handle this
                 print("pygame crash, AAAHHH")
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    print("Refreshing view of table")
+                    # PROPOSE MAKING UPDATING TABLE VIEW AN EVENT TRIGGERED BY USER.
+                    # TableView.Network_visibleCards()             
                 if event.key == pygame.K_9:
                     print("Drawing card")
                     self.controller.Draw()
+                    # seem to need to pause while waiting for drawn card to be returned?
+                    currentHand = self.controller.Get_Hand()
+                    if (len(currentHand) > 0):
+                        print(currentHand)
+                        newCard = currentHand[len(currentHand)-1]
+                        print(newCard._number)
+                        print(newCard._suit)
+                        card_XY = (10,10)
+                        img = ''
+                        card4gui = cardAndImage(newCard,card_XY,img)
+                        print(card4gui._card)
+                        print(card4gui._xy)
+                        print(card4gui._img)
                 if event.key == pygame.K_8:
                     print("Ending turn")
                     # self.controller.Discard(self.controller.Get_Hand())
@@ -70,4 +103,3 @@ class HandView():
             self.display.blit(text, textRect)
             textStartXY_wfeed = (textStartXY_wfeed[0],textStartXY_wfeed[1]+UIC.text_feed)
 
-        
