c6a1930da508faece05d10a179f3dcd8d3050aab
slwoodward <21129239+slwoodward@users.noreply.github.com>
2019-08-26 14:58:15 -0700

    Working on CardAndImage class
    Working on CardAndImage class -- perhaps this should be a dictionary, rather than a class??  Right now have a bug.

--- source/client/HandView.py
+++ source/client/HandView.py
@@ -9,18 +9,22 @@ from PodSixNet.Connection import connection, ConnectionListener
 class cardAndImage():     
         """GUI needs image and position of card"""
         def __init__(self, card, xy, img):
-            # should we check that card is in deck?
-            self._card = card
-            self._img = cardAndImage.get_image(self._card)
-            print('do NOT want get_image in render loop.')
-            self._xy = xy      
-        def get_image(self):
-            temp = self._suit
-            temp = temp[0]
-            imageFileName='client.card'+str(self._number)+ temp + '.png'
-            print(imageFileName)
-            img = 'imageFileName: '+imageFileName  # To DO load image and make this image
-            return(img)                                             
+                # should we check that card is in deck?
+                self._card = card
+                self._img = cardAndImage.get_image(self._card)
+                print('do NOT want get_image in render loop.')
+                self._xy = xy      
+        def get_image(onecard):
+                temp = onecard._suit
+                if(temp is not None):
+                        temp = temp[0]
+                else:
+                        temp = 'N'
+                imageFileName='client\card_images\card'+str(onecard._number)+ temp + '.png'
+                print(imageFileName)
+                img = pygame.image.load(imageFileName)
+                return(img)
+
 
 class HandView():
     """This class handles letting players actualy input information
@@ -38,16 +42,26 @@ class HandView():
         self.display = pygame.display.set_mode((UIC.displayWidth, UIC.displayHeight))
         pygame.display.set_caption(self.controller.Get_Name() + " View")
         self.display.fill(UIC.White)
-        # render starting window
+        # render starting window, 
         self.Render()
 
     def Render(self):
         """This should render the actual UI, for now it just prints the hand"""
         #TODO render the table view showing the visible cards
-        # 
+        # TO DO change screen split so top=table and bottom = hand (instead of side-by-side)
         currentHand = self.controller.Get_Hand()
         self.Print_Text("{0}".format(currentHand), (UIC.publicPrivateBoundary,0))
-        # ---
+        ''' temp values while creating Show_ - will eventually make next 2 variables lists of cardsandimages.....'''
+        selectedCards_Images = UIC.card4guiTests
+        heldCards_Images = UIC.card4guiTests
+        heldCards_Images._xy=(10,UIC.displayHeight*0.9)
+        self.Show_Selected(selectedCards_Images)
+        self.Show_Holding(heldCards_Images)
+        self.display.blit(UIC.backImg,(UIC.displayWidth/2,UIC.displayHeight/2))
+        '''
+        if(len(currentHand) > 1):
+           self.display.blit(card4gui._img,card4gui._xy)
+        '''
         pygame.display.update()
 
     def Next_Event(self):
@@ -66,18 +80,27 @@ class HandView():
                     print("Drawing card")
                     self.controller.Draw()
                     # seem to need to pause while waiting for drawn card to be returned?
+                    # NEED TO FIGURE THIS OUT.
                     currentHand = self.controller.Get_Hand()
+                    card_XY = (10,10)
                     if (len(currentHand) > 0):
                         print(currentHand)
-                        newCard = currentHand[len(currentHand)-1]
+                        tmp_indx=len(currentHand)-1
+                        newCard = currentHand[tmp_indx]
                         print(newCard._number)
                         print(newCard._suit)
-                        card_XY = (10,10)
-                        img = ''
+                        # card_XY = (card_XY[0]+30,card_XY[1]+30)
+                        img = UIC.backImg
                         card4gui = cardAndImage(newCard,card_XY,img)
                         print(card4gui._card)
                         print(card4gui._xy)
-                        print(card4gui._img)
+                        
+                        '''
+                        card4gui[tmp_indx] = cardAndImage(newCard,card_XY,img)
+                        print(card4gui[tmp_indx]._card)
+                        print(card4gui[tmp_indx]._xy)
+                        '''
+                        # print(card4gui._img)
                 if event.key == pygame.K_8:
                     print("Ending turn")
                     # self.controller.Discard(self.controller.Get_Hand())
@@ -89,6 +112,12 @@ class HandView():
                     else:
                         bogusDiscards = []
                     self.controller.Discard(bogusDiscards)
+
+    def Show_Holding(self,card4gui):
+        self.display.blit(card4gui._img,card4gui._xy)
+
+    def Show_Selected(self,card4gui):
+        self.display.blit(card4gui._img,card4gui._xy)
 
     def Print_Text(self, textString, textStartXY):
         """print the textString in a text box starting on the top left."""
@@ -102,4 +131,3 @@ class HandView():
             textRect.topleft = textStartXY_wfeed
             self.display.blit(text, textRect)
             textStartXY_wfeed = (textStartXY_wfeed[0],textStartXY_wfeed[1]+UIC.text_feed)
-
--- source/client/UIConstants.py
+++ source/client/UIConstants.py
@@ -4,6 +4,9 @@ Defines sizing, colors, font, and constant strings
 Has methods for importing and fetching images.
 """
 import pygame
+from common.Card import Card
+from client.HandView import cardAndImage
+
 #TODO: finalized choices are in Upper_Case, unfinalized may be lowercase
 
 # define colors (#rgb on scale of 0 to 255)
@@ -33,4 +36,16 @@ bigFontSz = 20
 bigText = pygame.font.Font("freesansbold.ttf",bigFontSz)
 text_feed = int(bigFontSz * 1.5)
 Wrap_Width = int(handColumnFraction * displayWidth / (bigFontSz * 0.5))
+
+# load image of back of card.
+backImg = pygame.image.load('client\card_images\cardBack.png')
+
+
+# Need card for testing.
+# for simplicity make it 3C (0 points), then change image to
+# backImg
+facedown=Card(3,'Clubs')
+card4guiTests = cardAndImage(facedown,(10,10),UIC.backImg)
+card4guiTests._img = UIC.backImg
+
 
