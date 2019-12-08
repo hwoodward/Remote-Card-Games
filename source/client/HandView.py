b19624936992924144416367a61e024996959269
slwoodward <21129239+slwoodward@users.noreply.github.com>
2019-12-07 18:27:59 -0800

    Prepared status created
    Changed outline on prepared cards, processed prepare selected cards button.  Haven't yet tested or worked on clear prepared cards button.

--- source/client/HandView.py
+++ source/client/HandView.py
@@ -102,13 +102,24 @@ class HandView:
                     # TODO: for sheri - move prepared even further to the right.
                     self.hand_info = self.refreshXY(self.hand_info)
                 if self.prepare_card_btn.isOver(pos):
-                    user_input_cards = self.controller.automaticallyPrepareCards(self.gatherSelected())
+                    already_prepared_cards = controller.getPreparedCards
+                    cardsToPrep = self.gatherSelected()
+                    user_input_cards = self.controller.automaticallyPrepareCards(cardsToPrep)
                     # The user_input_cards are a list of card/key option pairs ex. [[(0, None), [1,4,5,6,7,8,9,10,11,12,13]], [(2, 'Hearts'), [1,4,5,6,7,8,9,10,11,12,13]]]
                     # TODO: for Sheri - need to get user input on what key to prepare user_input_cards (wild cards) in.
                     # To prepare them when ready call self.controller.prepareCard(card, key)
                     #
                     # TODO: for Sheri - need to update display so that prepared cards are obvious...
                     # will probably move them vertically and might change outline color.
+                    #
+                    # newly_prepped_cards = all prepared cards minus already_prepared_cards
+                    newly_prepped_cards = controller.getPreparedCards
+                    for element in already_prepared_cards:
+                        newly_prepped_cards.remove(element)
+                    for wrappedcard in cardsToPrep:
+                        if wrappedcard.card in self.newly_prepped_cards:
+                            self.newly_prepped_cards.remove(wrappedcard.card)
+                            element.status == 2
                 if self.clear_prepared_cards_btn.isOver(pos):
                     self.controller.clearPreparedCards()
                 if self.play_prepared_cards_btn.isOver(pos):
@@ -118,7 +129,11 @@ class HandView:
                 else:
                     for element in self.hand_info:
                         if element.img_clickable.isOver(pos):
-                            if not element.status == 2:
+                            if element.status == 2:
+                                element.img_clickable.changeOutline(4)
+                                # prepared cards (status = 2) managed by controller.
+                                # to change prepared card's status must clear prepared cards.
+                            else:
                                 if element.status == 1:
                                     element.status = 0
                                     element.img_clickable.changeOutline(0)
