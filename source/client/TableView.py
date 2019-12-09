fe91678047e5a9ab33b2c7eb96e0917c4bcf4052
slwoodward <21129239+slwoodward@users.noreply.github.com>
2019-12-08 20:28:20 -0800

    Update TableView.py
    Table view much prettier than before.  Also runs more reliably with multiple players.

--- source/client/TableView.py
+++ source/client/TableView.py
@@ -30,32 +30,44 @@ class TableView(ConnectionListener):
         bk_grd_rect = (0, players_sp_top, players_sp_w, players_sp_h)
         for idx in range(len(self.hand_status)):
             player_name = self.player_names[idx]
-            # plan to move below to a helper function...
             melded_summary = self.compressed_info[player_name]
             pygame.draw.rect(self.display, UIC.table_grid_colors[color_index], bk_grd_rect, 0)
-            player_text = player_name
-            text_surface, text_rect = self.textObjects(player_text, UIC.Medium_Text)
+            if len(self.hand_status[idx]) > 1:
+                turnphase = self.hand_status[idx][0]
+                numcards = self.hand_status[idx][1]
+                foot = self.hand_status[idx][2]
+                player_text = player_name + ': ' + str(numcards)
+                if (foot > 0):
+                    player_text = player_text + ' cards (in hand)'
+                else:
+                    player_text = player_text + ' cards (in foot)'
+                if turnphase == 'inactive':
+                    text_surface, text_rect = self.textObjects(player_text, UIC.Medium_Text, UIC.Black)
+                else:
+                    text_surface, text_rect = self.textObjects(player_text, UIC.Big_Text, UIC.Black)
+            else:
+                player_text = player_name + ' (should be joining soon)'
+                text_surface, text_rect = self.textObjects(player_text, UIC.Medium_Text, UIC.Black)
             y_coord =  0.05 * UIC.Disp_Height
             text_rect.center = ((bk_grd_rect[0] + 0.5 * players_sp_w), (bk_grd_rect[1] + y_coord))
             y_coord = y_coord + UIC.Medium_Text_Feed
             self.display.blit(text_surface, text_rect)
             for key in melded_summary:
-                # player_text = player_text + ' ' + str(key) + ': ' + str(melded_summary[key]) + '\n'
-                i_key=int(key)
-                #todo sheri - see if I need to have variable i_key, or if key will work.
-                if i_key == 1:
+                if key == 1:
                     player_text = 'Aces: ' + str(melded_summary[key])
                     ykey = y_coord + (UIC.Small_Text_Feed * 11)
                 else:
-                    ykey = y_coord + (UIC.Small_Text_Feed * (i_key - 3))
+                    ykey = y_coord + (UIC.Small_Text_Feed * (key - 3))
                     player_text = str(key) + ': ' + str(melded_summary[key])
-                    if i_key == 11:
+                    if key == 11:
                         player_text = 'Jacks: ' + str(melded_summary[key])
-                    if i_key == 12:
+                    if key == 12:
                         player_text = 'Queens: ' + str(melded_summary[key])
-                    if i_key == 13:
+                    if key == 13:
                         player_text = 'Kings: ' + str(melded_summary[key])
-                text_surface, text_rect = self.textObjects(player_text, UIC.Small_Text)
+                text_surface, text_rect = self.textObjects(player_text, UIC.Small_Text, UIC.Black)
+                # todo: Sheri -- put in check on wilds, and make font red if clean and black if dirty.
+                # todo: Sheri -- denote canistas (additional text if melded summary shows >6 cards)
                 text_rect.center = ((bk_grd_rect[0] + 0.5 * players_sp_w), (bk_grd_rect[1] + ykey))
                 self.display.blit(text_surface, text_rect)
             # Move to next players rectangle and color:
@@ -81,8 +93,14 @@ class TableView(ConnectionListener):
                 summary[key] = (length_set, wild_count)
             self.compressed_info[key_player] = summary
 
+    '''
     def textObjects(self, text, font):
         text_surface = font.render(text, True, UIC.Black)
+        return text_surface, text_surface.get_rect()
+    '''
+
+    def textObjects(self, text, font, color):
+        text_surface = font.render(text, True, color)
         return text_surface, text_surface.get_rect()
 
     #######################################
