9fcd8dd1dcf3180d6270f2cb4eec842eb7341bc7
slwoodward <21129239+slwoodward@users.noreply.github.com>
2020-02-12 15:54:09 -0800

    More execution
    Executes until start game.

--- source/RunClient_beta01.py
+++ source/RunClient_beta01.py
@@ -1,6 +1,6 @@
 import sys
 from time import sleep
-from PodSixNet.Connection import connection
+from PodSixNet.Connection import connection, ConnectionListener
 from client.ClientState import ClientState
 from client.Controller import Controller
 from client.CreateDisplay import CreateDisplay
@@ -33,19 +33,32 @@ def RunClient():
     clientState = ClientState(ruleset)
     gameControl = Controller(clientState)
     playername = gameControl.getName()
+    print("DEBUG: reached point A in RunClient")
     createDisplay = CreateDisplay(playername)
+    print("DEBUG: reached point B in RunClient")
     handView = HandView(gameControl, createDisplay.display)
+    print("DEBUG: reached point C in RunClient")
     tableView = TableView(createDisplay.display)
+    print("DEBUG: reached point D in RunClient")
     while True:
         createDisplay.refresh()
+        print("DEBUG: reached point E in RunClient")
         handView.nextEvent()
+        print("DEBUG: reached point F in RunClient")
         connection.Pump()
+        print("DEBUG: reached point G in RunClient")
         gameControl.Pump()
+        print("DEBUG: reached point H in RunClient")
         tableView.Pump()
+        print("DEBUG: reached point I in RunClient")
         handView.update()
+        print("DEBUG: reached point J in RunClient")
         tableView.playerByPlayer()
+        print("DEBUG: reached point K in RunClient")
         note = gameControl.note
+        print("DEBUG: reached point L in RunClient")
         createDisplay.render(note)
+        print("DEBUG: reached point M in RunClient")
         sleep(0.001)
 
 if __name__ == "__main__":
