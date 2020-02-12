4c95d15e4773e0ffb9970c50f0dabd37c1b00df3
slwoodward <21129239+slwoodward@users.noreply.github.com>
2020-02-11 22:03:09 -0800

    Executable now connects
    Have gotten it to the point where executable will connect to the server and create display, but no images appear and it quickly closes.  Need to trace what it's crashing on now.

--- source/RunClient_beta01.py
+++ source/RunClient_beta01.py
@@ -1,12 +1,12 @@
 import sys
 from time import sleep
-from PodSixNet.Connection import connection, ConnectionListener
+from PodSixNet.Connection import connection
 from client.ClientState import ClientState
 from client.Controller import Controller
 from client.CreateDisplay import CreateDisplay
 from client.HandView import HandView
 from client.TableView import TableView
-# imports below make it added so that can generate executable using pyinstaller.
+# need to import the following here makes it easier to get pyinstaller to work.
 import common.HandAndFoot
 import common.Card
 import client.Button
@@ -22,11 +22,12 @@ def RunClient():
     
     It sets up the various classes and starts the game loop
     """
-    hostinfo = str(input("Enter the host:port[localhost:12345] ") or "localhost:12345")
-    host, port = hostinfo.split(":")
+    # host, port = sys.argv[1].split(":")
+    # ruleset = sys.argv[2]
+    host, port = "localhost", "12345"
+    ruleset = "HandAndFoot"
     print(host)
     print(port)
-    ruleset = str(input("Enter the ruleset[HandAndFoot] ") or "HandAndFoot")
     print(ruleset)
     connection.DoConnect((host, int(port)))
     clientState = ClientState(ruleset)
@@ -48,10 +49,13 @@ def RunClient():
         sleep(0.001)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 1:
-        print("This version gets host:port and RuleSet after starting.")
-        print("Do not include any arguments on command line")
-   else:
+    '''
+    if len(sys.argv) != 3:
+        print("Usage:", sys.argv[0], "host:port ruleset")
+        print("e.g.", sys.argv[0], "localhost:31425 HandAndFoot")
+    else:
         RunClient()
+    '''
+    RunClient()
 else:
     print("RunServer should not be imported anywhere")
---
+++ source/beta01/UIConstants.py
@@ -0,0 +1,59 @@
+""" These are the constants for the UI
+
+Defines sizing, colors, font, and constant strings
+Has methods for importing and fetching images.
+"""
+import pygame
+import os
+
+# define colors (#rgb on scale of 0 to 255)
+Black = (0,0,0)
+Gray = (100,100,100)
+White = (255,255,255)
+Red = (200,0,0)
+Green = (90,200,90)
+Yellow = (255,255,0)
+Bright_Yellow = (255,255,100)
+Blue = (0,0,200)
+Bright_Red = (255,0,0)
+Bright_Green = (0,255,0)
+Bright_Blue = (100,100,255)
+
+# Set display size
+# (below good for my laptop, screen resolution: 3840x2160, display set to 300%)
+Disp_Width = 1200
+Disp_Height = 600
+Hand_Row_Fraction = 0.3
+Hand_Col_Fraction = 1.0
+Table_Hand_Border = Disp_Height * (1 - Hand_Row_Fraction)
+
+# fonts
+pygame.font.init()
+Font_File = os.path.join('bundle_data', 'freesansbold.ttf')
+Small_Text = pygame.font.Font(Font_File, 14)
+Small_Text_Feed = int(14* 1.3)
+Medium_Text = pygame.font.Font(Font_File, 16)
+Medium_Text_Feed = int(16* 1.3)
+Big_Font_Sz = 20
+Big_Text = pygame.font.Font(Font_File, Big_Font_Sz)
+Text_Feed = int(Big_Font_Sz * 1.5)
+Wrap_Width = int(Hand_Col_Fraction * Disp_Width / (Big_Font_Sz * 0.5))
+table_grid_colors = ((180,199,231),(255,242,204),(197,224,180),(255,197,216),(192,192,192),(248,203,173))
+
+# card display
+scale = 0.7
+Card_Spacing = 70 * scale
+outline_width = 8 * scale
+no_outline_color = (-1, -1, -1)  # flags there is no outline.
+outline_colors = (no_outline_color, Yellow, Green, Bright_Green, Bright_Blue, Bright_Blue)
+# Since cannot change prepared cards status with mouse, don't highlight those cards when mouse is over them...
+# no_outline_color indicates clickable image (usually a card) not selected or prepared.
+# Yellow indicates not selected, but mouse is over clickable image.
+# green elements indicate card is 'selected'
+# bright green indicates mouse is over card.
+# blue indicates card is prepared (for play).
+
+# load image of back of card, and scale it.
+# Back_Img = pygame.image.load(os.path.join('client', 'cardimages', 'cardBack.png'))
+Back_Img = pygame.image.load(os.path.join('bundle_data', 'cardimages', 'cardBack.png'))
+Back_Img = pygame.transform.rotozoom(Back_Img, 0, scale)
